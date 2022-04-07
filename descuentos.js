553d358fd79b2515ab37fa0236b5e14ceebc3e5b
Andres Giraldo <andresgircar@gmail.com>
2022-04-07 16:55:33 -0500

    actualización archivos

--- descuentos.js
+++ descuentos.js
@@ -14,3 +14,13 @@ function calcularPrecioConDescuento (precio, descuento) {
     //porcentajeDescuento,
     //precioConDescuento
 //});
+
+function calcularPrecioFinal() {
+    const inputPrice = document.getElementById("inputPrice");
+    const priceValue= inputPrice.value;
+    const inputDiscount = document.getElementById("inputDiscount");
+    const discountValue= inputDiscount.value;
+    const precioFinal = calcularPrecioConDescuento(priceValue, discountValue);
+    const resultP = document.getElementById("ResultP");
+    resultP.innerText = "El precio que debes pagar después de aplicar el descuento es: $" + precioFinal;
+}
